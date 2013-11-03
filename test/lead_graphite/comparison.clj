4ee5d27046b62907ed7edc79d5469ec77caa5928
Ryan Berdeen <ryan@ryanberdeen.com>
2013-11-03 04:28:23 -0500

    fix value comparison, add tests

--- test/lead_graphite/comparison.clj
+++ test/lead_graphite/comparison.clj
@@ -8,6 +8,7 @@
             [lead-graphite.graphite :as graphite]))
 
 (def num-tests 100)
+(def epsilon 10e-6)
 
 (defrecord TestSeriesSource [serieses]
   fns/SeriesSource
@@ -28,26 +29,37 @@
 
 (defn close-enough? [serieses-a serieses-b]
   (every? (fn [[series-a series-b]]
-            (every? (fn [[a b]] (= (double a) (double b))) (map vector (:values series-a) (:values series-b))))
+            (every? (fn [[a b]] (or (= a b) ; handles nil
+                                    (< (Math/abs (- (double a) (double b))) epsilon)))
+                    (map vector (:values series-a) (:values series-b))))
           (map vector serieses-a serieses-b)))
+
+(defn run [s lead-f graphite-f args]
+  (let [args (cons s args)
+        opts {:start (-> s :serieses first :start) :end (-> s :serieses first :end)}
+        lead-source (fns/function->source "test" lead-f args)
+        graphite-source (fns/function->source "test" graphite-f args)
+        lead-result (fns/load-serieses lead-source opts)
+        graphite-result (fns/load-serieses graphite-source opts)]
+    [lead-result graphite-result]))
 
 (defn compare-source [s lead-f graphite-f args]
   (try
-    (let [args (conj args s)
-          opts {:start (-> s :serieses first :start) :end (-> s :serieses first :end)}
-          lead-source (fns/function->source "test" lead-f args)
-          graphite-source (fns/function->source "test" graphite-f args)
-          lead-result (fns/load-serieses lead-source opts)
-          graphite-result (fns/load-serieses graphite-source opts)]
+    (let [[lead-result graphite-result] (run s lead-f graphite-f args)]
       (close-enough? lead-result graphite-result))
     (catch Exception e
       (.printStackTrace e)
       false)))
 
 (defn compare-serieses [lead-f graphite-f & args]
-  (prop/for-all [s gen-serieses-source]
+  (prop/for-all [s gen-serieses-source
+                 args (apply gen/tuple args)]
     (compare-source s lead-f graphite-f args)))
 
-(defspec test-scale num-tests (compare-serieses #'lead/scale-serieses #'graphite/scale 2))
-(defspec test-offset num-tests (compare-serieses #'lead/increment-serieses #'graphite/offset 2))
-(defspec test-avg num-tests (compare-serieses #'lead/avg-serieses #'graphite/minSeries))
+(defspec test-scale num-tests (compare-serieses #'lead/scale-serieses #'graphite/scale gen/int))
+(defspec test-offset num-tests (compare-serieses #'lead/increment-serieses #'graphite/offset gen/int))
+(defspec test-avg num-tests (compare-serieses #'lead/avg-serieses #'graphite/avg))
+(defspec test-min num-tests (compare-serieses #'lead/min-serieses #'graphite/minSeries))
+(defspec test-max num-tests (compare-serieses #'lead/max-serieses #'graphite/maxSeries))
+(defspec test-sum num-tests (compare-serieses #'lead/sum-serieses #'graphite/sumSeries))
+(defspec test-removeBelowValue num-tests (compare-serieses #'lead/map-values-below-to-nil #'graphite/removeBelowValue gen/int))
