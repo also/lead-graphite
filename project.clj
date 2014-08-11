0a12986ac2aa5f3bc0a364b96aa6f4602d517752
Ryan Berdeen <ryan@ryanberdeen.com>
2014-08-11 09:01:21 -0400

    upgrade jython

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :dependencies [
                  [com.ryanberdeen/lead "0.1.0-SNAPSHOT"]
                  [org.clojure/clojure "1.5.1"]
-                 [org.python/jython-standalone "2.7-b1"]
+                 [org.python/jython-standalone "2.7-b2"]
                  [reiddraper/simple-check "0.5.2" :scope "test"]]
   :aliases {"graphite" ["with-profile" "graphite" "test"]}
   :profiles {:graphite {:jvm-opts ["-Dpython.path=graphite-web/webapp"]}})
